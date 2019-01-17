a1b563b2e66038a9340b274f1beb3e72c297e283
w770762632 <37448547+w770762632@users.noreply.github.com>
2019-01-17 22:09:38 +0800

    Update homepage.py

--- zsjj/page/homepage.py
+++ zsjj/page/homepage.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from common.base import BasePage
 
-homepage_url='https://direct.cmfchina.com/ECApp/ECHome/applicationGroups.jsp?mainCatId=ACCOUNT_INFO&thirdCatId=ACCOUNT_INFO_HOME'
+homepage_url='https://direct.******.com/ECApp/ECHome/applicationGroups.jsp?mainCatId=ACCOUNT_INFO&thirdCatId=ACCOUNT_INFO_HOME'
 
 class HomePage(BasePage):
     name_loc=('id','header_username')
